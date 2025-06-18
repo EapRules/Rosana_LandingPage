bf3fd9291f38e0c2beaaa66c37529dc23909c159
Enzo Politi <enzoapoliti@gmail.com>
2025-06-18 13:49:59 -0300

    Updated Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -32,7 +32,7 @@ export const Navbar = () => {
         <div className="flex justify-center mt-4 sm:mt-6 md:mt-8 mb-4 sm:mb-6 md:mb-8">
           <div className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-poppins font-semibold pointer-events-auto">
             <span className="text-white drop-shadow-lg">RH</span>
-            <span className="text-white drop-shadow-lg color-bg-brand-fucsia">S/></span>
+            <span className="text-white drop-shadow-lg color-brand-fucsia">S/></span>
           </div>
         </div>
       </div>
